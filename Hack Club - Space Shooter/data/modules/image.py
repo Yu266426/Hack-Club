ced51b7dd3554f7fc9138353f83375fb7dab0e52
Yu266426 <yu266426@gmail.com>
2023-01-20 11:21:11 -0800

    Minor fixes

--- Hack Club - Space Shooter/data/modules/image.py
+++ Hack Club - Space Shooter/data/modules/image.py
@@ -9,7 +9,7 @@ class Image:
 	def __init__(self, name, scale, angle):
 		self.angle = angle
 
-		self.base_image = pygame.image.load(os.path.join(ASSET_DIR, f"{name}.png"))
+		self.base_image = pygame.image.load(os.path.join(ASSET_DIR, f"{name}.png")).convert_alpha()
 		self.base_image = pygame.transform.scale(self.base_image, (self.base_image.get_width() * scale, self.base_image.get_height() * scale))
 		self.image = pygame.transform.rotate(self.base_image, self.angle)
 
@@ -21,5 +21,5 @@ class Image:
 		return self.base_image.get_rect(center=pos)
 
 	def draw(self, window: pygame.Surface, pos):
-		# Draw image centered on the center of the base image
+		# Draw image centered on center of the base image
 		window.blit(self.image, self.image.get_rect(center=self.get_rect(pos).center))
