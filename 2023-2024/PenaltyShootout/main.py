f04650610b81d48089906419055ea2c648573920
Yu266426 <yu266426@gmail.com>
2023-11-08 12:55:17 -0800

    Nov 8 Meeting

--- 2023-2024/PenaltyShootout/main.py
+++ 2023-2024/PenaltyShootout/main.py
@@ -21,10 +21,44 @@ class Player:
 		pygame.draw.circle(surface, "white", self.pos, 20)
 
 
+class Ball:
+	def __init__(self, pos, velocity):
+		self.pos = pygame.Vector2(pos)
+
+		self.velocity = pygame.Vector2(velocity)
+
+		self.image = pygame.image.load("soccer_ball.png")
+		self.image = pygame.transform.scale(self.image, (40, 40))
+
+		self.rect = self.image.get_rect(center=self.pos)
+
+	def update(self):
+		self.pos.x += self.velocity.x
+		self.rect.center = self.pos
+
+		if self.rect.left < 0:
+			self.velocity.x *= -1
+		if self.rect.right > 600:
+			self.velocity.x *= -1
+
+		self.pos.y += self.velocity.y
+		self.rect.center = self.pos
+
+		if self.rect.top < 0:
+			self.velocity.y *= -1
+		if self.rect.bottom > 600:
+			self.velocity.y *= -1
+
+	def draw(self, surface: pygame.Surface):
+		surface.blit(self.image, self.rect)
+
+
 screen = pygame.display.set_mode((600, 600))
 clock = pygame.Clock()
 
 player = Player()
+
+ball = Ball((300, 400), (-2.4, 7.3))
 
 running = True
 while running:
@@ -38,10 +72,14 @@ while running:
 			if event.key == pygame.K_ESCAPE:
 				running = False
 
+	# Updating
 	player.move()
+	ball.update()
 
+	# Drawing
 	screen.fill("dark green")
 
 	player.draw(screen)
+	ball.draw(screen)
 
 	pygame.display.flip()
